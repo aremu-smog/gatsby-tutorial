56eb97cbbcd4bbd6051a9fdd81e6aa2427d6f0a0
aremu-smog <aremuoluwagbamila@gmail.com>
2020-10-26 16:45:42 +0000

    Custom 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -6,8 +6,7 @@ import SEO from "../components/seo"
 const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found" />
-    <h1>404: Not Found</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <h1>Hello o, wetin you find reach here?</h1>
   </Layout>
 )
 
